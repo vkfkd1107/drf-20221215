fedb1712d2831afced6dfa732bd32cc558fd77b7
vkfkd1107 <ji_su1107@naver.com>
2022-12-15 21:43:38 +0900

    APIView Class로 구성된 투두리스트

--- mysite/todo/urls.py
+++ mysite/todo/urls.py
@@ -3,8 +3,9 @@ from rest_framework.routers import DefaultRouter
 from todo import views
 
 router = DefaultRouter()
-router.register('todo', views.TodoViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('todo-list', views.TodoList.as_view(), name='todo_list'),
+    path('todo/<int:pk>/', views.TodoDetail.as_view(), name='todo_detail'),
 ]
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -1,15 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.views import APIView
 from todo.models import Todo
+from rest_framework import status
+from rest_framework.response import Response
 from todo.serializers import TodoSerializer
 
 
-class TodoViewSet(ModelViewSet):
-    queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+# First: class APIView를 기반으로 구현하기
+class TodoList(APIView):
+    def get(self, request, format=None):
+        qs = Todo.objects.all()
+        serializer = TodoSerializer(qs, many=True)
+        return Response(serializer.data)
+    def post(self, request, format=None):
+        serializer = TodoSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        else:
+            return Response(serializer.data, status=400)
 
-    # dispatch
-    def dispatch(self, request, *args, **kwargs):
-        print("request.body: ", request.body)
-        print("request.POST: ", request.POST)
-        return super().dispatch(request, *args, **kwargs)
+
+class TodoDetail(APIView):
+    def get_object(self, pk):
+        return get_object_or_404(Todo, pk=pk)
+    def get(self, request, pk, format=None):
+        todo = self.get_object(pk)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+    def put(self, request, pk, format=None):
+        todo = self.get_object(pk)
+        serializer = TodoSerializer(todo, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.error)
+    def delete(self, request, pk, format=None):
+        todo = self.get_object(pk)
+        print(todo)
+        todo.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
