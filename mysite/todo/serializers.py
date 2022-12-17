92aedb10d1ced0ef2cec2e11a14fbd79cea73d01
vkfkd1107 <ji_su1107@naver.com>
2022-12-17 18:37:54 +0900

    [Add] username, useremail at TodoSerialier by ReadyOnlyField

--- mysite/todo/serializers.py
+++ mysite/todo/serializers.py
@@ -3,6 +3,8 @@ from todo.models import Todo
 
 
 class TodoSerializer(serializers.ModelSerializer):
+    username = serializers.ReadOnlyField(source='user.username')
+    useremail = serializers.ReadOnlyField(source='user.email')
     class Meta:
         model = Todo
         fields = '__all__'
