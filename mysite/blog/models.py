cc885dc0d84846c07ba8dc765b0154dede7cefc8
vkfkd1107 <ji_su1107@naver.com>
2022-12-17 18:27:37 +0900

    [Add] user foreign key: Post model, Todo model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 
 class Post(models.Model):
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
     title = models.CharField(blank=True, max_length=120)
     context = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- mysite/todo/models.py
+++ mysite/todo/models.py
@@ -1,7 +1,14 @@
 from django.db import models
+from django.conf import settings
 
 
 class Todo(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
     title = models.CharField(blank=True, max_length=120)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
