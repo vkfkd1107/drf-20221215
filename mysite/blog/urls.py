13cbe647f233b574c39a3fc15f3e47608b2ce97f
vkfkd1107 <ji_su1107@naver.com>
2022-12-27 20:57:29 +0900

    blog app function view renderer 구현

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from blog import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
     path('post', views.PostList.as_view(), name='post'),
     path('post/<int:pk>', views.PostDetail.as_view(), name='post'),
+    path('static_view', views.static_view, name='renderer_classes'),
+    path('format_view_blog', views.format_view_blog, name='format_view_blog'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -4,9 +4,9 @@ from blog.models import Post
 from blog.serializers import PostSerializer
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import generics, mixins, viewsets
-from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.renderers import TemplateHTMLRenderer, StaticHTMLRenderer
 
 
 # template html renderer 구현
@@ -29,3 +29,15 @@ class PostDetail(generics.RetrieveAPIView):
         return Response({
             'post': self.get_object()
         })
+
+
+@api_view(['GET'])
+@renderer_classes([StaticHTMLRenderer])
+def static_view(request):
+    data = "<html><body><h1>Hello, Here is Blog App</h1></body></html>"
+    return Response(data)
+
+
+@api_view(['GET'])
+def format_view_blog(request, format=None):
+    return Response([])
