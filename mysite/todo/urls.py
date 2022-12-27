d040575996011a393216822f81dc31b471cd425f
vkfkd1107 <ji_su1107@naver.com>
2022-12-27 20:37:10 +0900

    Todo template renderer 구현

--- mysite/todo/urls.py
+++ mysite/todo/urls.py
@@ -1,10 +1,13 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from todo import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
-router = DefaultRouter()
-router.register('todo', views.TodoModelViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('post/', views.TodoList.as_view(), name='post'),
+    path('post/<int:pk>', views.TodoDetail.as_view(), name='post'),
+    path('static_html_renderer', views.static_html_renderer, name='static_html_renderer'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- mysite/todo/views.py
+++ mysite/todo/views.py
@@ -5,12 +5,37 @@ from todo.models import Todo
 from rest_framework import status
 from rest_framework.response import Response
 from todo.serializers import TodoSerializer
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework.renderers import TemplateHTMLRenderer, StaticHTMLRenderer
 
 
-# viewset 구현
-class TodoModelViewSet(ModelViewSet):
+# tempate renderer 구현
+class TodoList(generics.ListCreateAPIView):
     queryset = Todo.objects.all()
-    serializer_class = TodoSerializer
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'todo_list.html'
+
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'posts': self.get_queryset()
+        })
+
+
+class TodoDetail(generics.RetrieveAPIView):
+    queryset = Todo.objects.all()
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'todo.html'
+
+    def get(self, request, *args, **kwargs):
+        return Response({
+            'post': self.get_object()
+        })
+
+
+@api_view(['GET'])
+@renderer_classes([StaticHTMLRenderer])
+def static_html_renderer(request):
+    data = "<html><body><h1>Hello, Here is Todo App</h1></body></html>"
+    return Response(data)
